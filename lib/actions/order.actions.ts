fba7a45622efbd462e45e7506ebe0837a7d44862
harshit7905 <harshit.20214269@mnnit.ac.in>
2024-03-27 18:21:10 +0530

    removed

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -34,10 +34,8 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
         buyerId: order.buyerId,
       },
       mode: 'payment',
-      // success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-      // cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/success`,
-      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cancel`,
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
 
     redirect(session.url!)
